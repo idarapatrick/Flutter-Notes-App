a830d8fd57190c07a1a269df93ba01c1de20170c
idarapatrick <essienidara67@gmail.com>
2025-07-06 17:57:14 +0200

    New bloc item - theme

---
+++ lib/presentation/blocs/theme/theme_cubit.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeCubit extends Cubit<ThemeMode> {
+  static const _key = 'theme_mode';
+
+  ThemeCubit() : super(ThemeMode.system) {
+    _loadTheme();
+  }
+
+  void _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final mode = prefs.getString(_key);
+    if (mode == 'light')
+      emit(ThemeMode.light);
+    else if (mode == 'dark')
+      emit(ThemeMode.dark);
+    else
+      emit(ThemeMode.system);
+  }
+
+  void setTheme(ThemeMode mode) async {
+    emit(mode);
+    final prefs = await SharedPreferences.getInstance();
+    if (mode == ThemeMode.light)
+      prefs.setString(_key, 'light');
+    else if (mode == ThemeMode.dark)
+      prefs.setString(_key, 'dark');
+    else
+      prefs.remove(_key);
+  }
+}
